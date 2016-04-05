a7a87705695c44eb22fb386c8b22ce9039646975
Pat Yolian <pyolian@gmail.com>
2016-04-04 19:49:34 -0700

    Start work on handlebars template, finish AJAX request

--- scripts/repoRequest.js
+++ scripts/repoRequest.js
@@ -1,6 +1,8 @@
 (function(module) {
   var repo = {};
 
+  repo.all = [];
+
   repo.requestRepos = function() {
     $.ajax({
       url: 'https://api.github.com/users/patSSFC/repos',
@@ -8,9 +10,13 @@
         'Authorization': authToken,
       },
       success: function(data, message, xhr) {
-        console.log(data);
+        // console.log(data);
+        repo.all = data;
       }
     });
   };
 
+  
+
+  this.repo = repo;
 })(window);
