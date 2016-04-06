46afd3b4ac65a9dbba59a38685397523b65d040f
Pat Yolian <pyolian@gmail.com>
2016-04-06 09:43:02 -0700

    add repoController link to index, add /repo to routes.js

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -3,5 +3,6 @@ console.log('yo!!');
 page('/', introController.showIntro);
 page('/projects', projectsController.showProjects);
 page('/contact', contactController.showContact);
+page('/repos', repoController.showRepos);
 
 page();
