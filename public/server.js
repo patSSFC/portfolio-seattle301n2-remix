ad4829d6cab6f36fe33462c49f4250968ab374c0
Pat Yolian <pyolian@gmail.com>
2016-04-06 08:14:17 -0700

    fix typo in server.js

--- public/server.js
+++ public/server.js
@@ -30,7 +30,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req,res) {
   console.log(req.body);
-  response.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(port, function() {
   console.log('Server running on...' + port);
