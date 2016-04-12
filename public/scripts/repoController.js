0e1a93d229b928ce5e8eb34ffdc7f436d74c4273
Pat Yolian <pyolian@gmail.com>
2016-04-11 22:39:21 -0700

    get hide repo-details to hide, start to style repos

--- public/scripts/repoController.js
+++ public/scripts/repoController.js
@@ -5,10 +5,10 @@
   repoController.showRepos = function() {
     $target = $('.tab-content');
     $target.hide().filter('#repos').show();
+    $('.repo-details').hide();
   };
 
   repoController.showTeaser = function() {
-    console.log($('#repos').find('.repo').children());
 
     $('#repos').on('click', '.repo-name', function() {
       $(this).siblings().slideToggle();
