90b8d736723226044e704b9a4f2e60812756d6d1
Pat Yolian <pyolian@gmail.com>
2016-04-26 14:23:26 -0700

    fix issue of reprinting repo list, now clears children before printing

--- public/scripts/repoController.js
+++ public/scripts/repoController.js
@@ -5,10 +5,10 @@
     repo.requestRepos(repo.initPage);
     $target = $('.tab-content');
     $target.hide().filter('#repos').show();
+    $('#repos').empty();
   };
 
   repoController.showTeaser = function() {
-
     $('#repos').on('click', '.repo-name', function() {
       $(this).siblings().slideToggle();
     });
